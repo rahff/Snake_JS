a41a9397b414bbdc9e3968accd435e50488f4f16
rahff <raphaelandrey99@gmail.com>
2024-04-04 14:30:33 +0200

    remove useless testing on frontend

--- Frontend/src/home/main.js
+++ Frontend/src/home/main.js
@@ -1,13 +1,12 @@
 import {participate_action, participate} from "./participate.js";
 
+const PARTICIPATE_BTNS = 'button[data-action="participate"]';
 
 const init_page_reactivity = (fetch, dom_api) => _ => {
-    dom_api.querySelectorAll('button[data-action="participate"]')
-        .forEach(btn => {
+    dom_api.querySelectorAll(PARTICIPATE_BTNS).forEach(btn => {
         btn.addEventListener('click', participate_action(participate(fetch)))
     })
 }
-
 
 addEventListener("DOMContentLoaded", init_page_reactivity(fetch, document));
 
